b5768713af8db029ed72fb54b7be6309ad69df7a
Babak Aghili <babakaghili@icloud.com>
2014-11-15 16:41:29 -0600

    Chapter 7 Stuff

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver:IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            this.kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product {Name = "Football", Price = 25},
+            //    new Product {Name = "Surf Board", Price = 150},
+            //    new Product {Name = "Running Shoes", Price = 96}
+            //});
+
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
